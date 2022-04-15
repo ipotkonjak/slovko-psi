06ff747dc322d6bd916daa987df66a6d72983020
katarinajj <katarinajocic00@gmail.com>
2022-04-16 01:38:53 +0200

    Lepsi izgled mape i dodata poruka o neuspehu.

--- Klijent/scripts/singleplayer.js
+++ Klijent/scripts/singleplayer.js
@@ -6,22 +6,19 @@ let secretWord = "одмор";
 let enteredWord = [];
 let currentRow = 1;
 let currentCol = 1;
+let guess = 0;
 var enableEntry = true;
-var won = false;
+var gameOver = false;
 var numOfGuesses = 6;
 
-const letterMap = new Map();
-
-letterMap.set('а', 1); letterMap.set('б', 2); letterMap.set('в', 3);
-letterMap.set('г', 4); letterMap.set('д', 5); letterMap.set('ђ', 6);
-letterMap.set('е', 7); letterMap.set('ж', 8); letterMap.set('з', 9); 
-letterMap.set('и', 10); letterMap.set('ј', 11); letterMap.set('к', 12);
-letterMap.set('л', 13); letterMap.set('љ', 14); letterMap.set('м', 15);
-letterMap.set('н', 16); letterMap.set('њ', 17); letterMap.set('о', 18); 
-letterMap.set('п', 19); letterMap.set('р', 20); letterMap.set('с', 21); 
-letterMap.set('т', 22); letterMap.set('ћ', 23); letterMap.set('у', 24);
-letterMap.set('ф', 25); letterMap.set('х', 26); letterMap.set('ц', 27);
-letterMap.set('ч', 28); letterMap.set('џ', 29); letterMap.set('ш', 30);
+const letterMap = {
+    "а" : 1, "б" : 2, "в" : 3, "г" : 4, "д" : 5, 
+    "ђ" : 6, "е" : 7, "ж" : 8, "з" : 9, "и" : 10, 
+    "ј" : 11, "к" : 12, "л" : 13, "љ" : 14, "м" : 15, 
+    "н" : 16, "њ" : 17, "о" : 18, "п" : 19, "р" : 20, 
+    "с" : 21, "т" : 22, "ћ" : 23, "у" : 24, "ф" : 25, 
+    "х" : 26, "ц" : 27, "ч" : 28, "џ" : 29, "ш" : 30 
+}
 
 const keyMap = {
     "KeyA" : "а", "KeyB" : "б", "KeyV" : "в", "KeyG" : "г", "KeyD" : "д", "BracketRight" : "ђ", 
@@ -41,7 +38,7 @@ function copyArray(arr){
 
 function fillCount() {
     for (let i = 0; i < 5; i++) {
-        let ind = letterMap.get(secretWord[i]);
+        let ind = letterMap[secretWord[i]];
         count[ind - 1] += 1;
     }
 }
@@ -54,24 +51,13 @@ document.addEventListener("keydown", function (event) {
     if (letter) enter(letter);
     else if (event.code == "Enter") checkEnteredWord();
     else if (event.code == "Backspace") removeLetter();
-
-    // switch(event.code) {
-    //     case 'KeyA' : enter('а'); break;
-    //     case 'KeyA' : enter('б'); break;
-    //     case 'KeyA' : enter('в'); break;
-    //     case 'KeyA' : enter('г'); break;
-    //     case 'KeyA' : enter('д'); break;
-        
-
-    // }
-
     
     event.preventDefault();
   }, true);
 
 
 function enter(currentLetter) { // we put letter in (currentRow, currentCol)
-    if (won) return;
+    if (gameOver) return;
     if (currentRow == numOfGuesses && currentCol == 6) return;
     if (enableEntry == false) return;
     if (currentCol == 6) {
@@ -92,7 +78,7 @@ function enter(currentLetter) { // we put letter in (currentRow, currentCol)
 }
 
 function removeLetter() { 
-    if (won) return;
+    if (gameOver) return;
     if (currentRow == 1 && currentCol == 1) return; // empty
     if (currentCol == 6 && enableEntry == true) return; // UNESI was clicked
     currentCol -= 1;
@@ -109,8 +95,9 @@ function removeLetter() {
 }
 
 function checkEnteredWord() {
-    if (won) return;
+    if (gameOver) return;
     if (currentCol != 6) return;
+    guess++;
     let greens = 0;
     (colored = []).length = 5; colored.fill(false);
 		
@@ -119,7 +106,7 @@ function checkEnteredWord() {
             document.getElementById(enteredWord[i].square).style.backgroundColor = colorGreen;
             document.getElementById(enteredWord[i].letter).style.backgroundColor = colorGreen;
             greens++;   
-            count[letterMap.get(secretWord[i]) - 1] -= 1;   
+            count[letterMap[secretWord[i]] - 1] -= 1;   
             colored[i] = true;      
             continue;
         }     
@@ -129,7 +116,7 @@ function checkEnteredWord() {
 
     for(let i = 0; i < 5; i++) { 
         if (colored[i]) continue;
-        let ind = letterMap.get(enteredWord[i].letter);
+        let ind = letterMap[enteredWord[i].letter];
         if (copyCount[ind - 1] > 0) {
             document.getElementById(enteredWord[i].square).style.backgroundColor = colorPink;
             let keyboard = document.getElementById(enteredWord[i].letter);
@@ -148,9 +135,18 @@ function checkEnteredWord() {
         colored[i] = true;
     }
 
-    if (greens < 5) { enableEntry = true; return; }
-    won = true;
-    setTimeout(function() {
-        alert("Свака част!");
-    }, 1000);
+    if (greens == 5) {
+        gameOver = true;
+        setTimeout(function() {
+            alert("Свака част!");
+        }, 1000);
+    }
+    else if (guess == numOfGuesses) {
+        gameOver = true;
+        setTimeout(function() {
+            alert("Тражена реч је била " + secretWord + "!");
+        }, 1000);
+    }
+    else { enableEntry = true; }
+    
 }
