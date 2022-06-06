2f85483db13ea2d1e463610b38436dc4b6723958
katarinajj <katarinajocic00@gmail.com>
2022-06-06 19:20:10 +0200

    Update obradaVipZahtev -> los format

--- Server/slovko/app/Views/stranice/obradaVIPzahtev.php
+++ Server/slovko/app/Views/stranice/obradaVIPzahtev.php
@@ -6,9 +6,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="/assets/css/pregled.css" type="text/css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+    <link rel="stylesheet" href="/assets/css/pregled.css" type="text/css">
     <title>Преглед профила одабраног корисника</title>
 </head>
 
@@ -25,70 +25,63 @@
             <div class="title-center">
                 <br>
                 <h3>Преглед корисника за обраду</h3>
-                <br>
             </div>
             <div class="title-right">  </div>
         </div>
 
-        
-        
-        
         <div class="info">
-            <table class="styled-info">
-                <tr>
-                    <th>Kорисничко име</th>
-                    <td><?php echo $korisnik->username; ?></td>
-                </tr>
-                <tr>
-                    <th>Име</th>
-                    <td><?php echo $korisnik->ime; ?></td>
-                </tr>
-                <tr>
-                    <th>Презиме</th>
-                    <td><?php echo $korisnik->prezime; ?></td>
-                </tr>
-                <tr>
-                    <th>e-mail</th>
-                    <td><?php echo $korisnik->email; ?></td>
-                </tr>
-            </table>
-            <br>
-            <hr>
-            <br>
-            <table class="styled-info">
-                <thead>
+                <table class="styled-info" id="info">
                     <tr>
-                        <th id="stat" colspan="2">статистика</th>
+                        <th>Kорисничко име</th>
+                        <td><?php echo $korisnik->username; ?></td>
                     </tr>
-                </thead>
-                <tr>
-                    <td>Број поена</td>
-                    <td><?php echo $statistika->brojPoena;?></td>
-                </tr>
-                <tr>
-                    <td>Број победа</td>
-                    <td><?php echo $statistika->brojPobeda;?></td>
-                </tr>
-                <tr>
-                    <td>Број нерешених</td>
-                    <td><?php echo $statistika->brojNeresenih;?></td>
-                </tr>
-                <tr>
-                    <td>Број пораза</td>
-                    <td><?php echo $statistika->brojPoraza;?></td>
-                </tr>
-                <tr>
-                    <td>Успех - аркадна игра</td>
-                    <td><?php echo $statistika->arcadeRecord;?></td>
-                </tr>
-                <tr>
-                    <td>Позиција на ранг листи</td>
-                    <td>1</td> <!-- Dodati rang -->
-                </tr>
-
-            </table>
-
-        </div>
+                    <tr>
+                        <th>Име</th>
+                        <td><?php echo $korisnik->ime; ?></td>
+                    </tr>
+                    <tr>
+                        <th>Презиме</th>
+                        <td><?php echo $korisnik->prezime; ?></td>
+                    </tr>
+                    <tr>
+                        <th>e-mail</th>
+                        <td><?php echo $korisnik->email; ?></td>
+                    </tr>
+                </table>
+                <br>
+                <table class="styled-info">
+                    <thead>
+                        <tr>
+                            <th id="stat" colspan="2">СТАТИСТИКА</th>
+                        </tr>
+                    </thead>
+                    <tr>
+                        <th>Број поена</td>
+                        <td><?php echo $statistika->brojPoena;?></td>
+                    </tr>
+                    <tr>
+                        <th>Број победа</td>
+                        <td><?php echo $statistika->brojPobeda;?></td>
+                    </tr>
+                    <tr>
+                        <th>Број нерешених</td>
+                        <td><?php echo $statistika->brojNeresenih;?></td>
+                    </tr>
+                    <tr>
+                        <th>Број пораза</td>
+                        <td><?php echo $statistika->brojPoraza;?></td>
+                    </tr>
+                    <tr>
+                        <th>Успех - аркадна игра</td>
+                        <td><?php echo $statistika->arcadeRecord;?></td>
+                    </tr>
+                    
+                </table>
+                
+            </div> 
+        
+        
+        
         <div class = "vipZahtev">
             <form action="<?php echo site_url("Admin/vipObrada/".$korisnik->username); ?>" method="post">
                 Захтев прихваћен: <input type="checkbox" name="prihvacen"><br>
